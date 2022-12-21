0e3f9e5954f138de99cc3d72a36a79baea200188
Ivan <kozhevnikovivan86@gmail.com>
2022-12-21 14:14:58 +0300

    react to user action on button click-2

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,10 +1,10 @@
 const slider = (nodeSlider) => {
   const backButton = document.querySelector('.slider__button--prev');
   const nextButton = document.querySelector('.slider__button--next');
-  backButton.addEventListener('click', function() {
+  backButton.addEventListener('click', () => {
     console.log("click on prev button")
   })
-  nextButton.addEventListener('click', function() {
+  nextButton.addEventListener('click', () => {
     console.log("click on next button")
   })
 }
