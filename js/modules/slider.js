8b2b1a97c4b8aea3dfee6d669e03ad594b5ec5fe
Ivan <kozhevnikovivan86@gmail.com>
2022-12-23 23:12:59 +0300

    scrolling the carousel modified-2

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,22 +1,23 @@
 const STEP = 1;
 const FORWARD_DIRECTION = -1;
 const BACK_DIRECTION = 1;
+
 const slider = (nodeSlider) => {
   const backButton = nodeSlider.querySelector('.slider__button--prev');
   const nextButton = nodeSlider.querySelector('.slider__button--next');
   const sliderList = nodeSlider.querySelector('.slider__list');
   const sliderItem = nodeSlider.querySelector('.goods__item');
-  const sliderWidth = sliderItem.scrollWidth;
+  const sliderWidth = sliderItem.offsetWidth;
 
-  const findCoordinate = (step, direction) => {
+  const findUseCoordinate = (step, direction) => {
    const sliderCoordinate = `${step * direction * sliderWidth}px`;
    return sliderList.style.transform += `translateX(${sliderCoordinate})`;
   }
   backButton.addEventListener('click', () => {
-    findCoordinate(STEP, BACK_DIRECTION);
+    findUseCoordinate(STEP, BACK_DIRECTION);
   })
   nextButton.addEventListener('click', () => {
-    findCoordinate(STEP, FORWARD_DIRECTION);
+    findUseCoordinate(STEP, FORWARD_DIRECTION);
   })
 }
 
