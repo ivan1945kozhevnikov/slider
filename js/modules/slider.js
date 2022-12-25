5751cbce0d301b787e8c68b3c4817e6f533a6a83
Ivan <kozhevnikovivan86@gmail.com>
2022-12-25 18:49:01 +0300

    control the state of the buttons while scrolling the carousel

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -1,6 +1,7 @@
 const STEP = 1;
 const FORWARD_DIRECTION = -1;
 const BACK_DIRECTION = 1;
+const START_POSITION = 0;
 
 const slider = (nodeSlider) => {
   const backButton = nodeSlider.querySelector('.slider__button--prev');
@@ -9,15 +10,28 @@ const slider = (nodeSlider) => {
   const sliderItem = nodeSlider.querySelector('.goods__item');
   const sliderWidth = sliderItem.offsetWidth;
 
-  const setCoordinate = (step, direction) => {
-   const sliderCoordinate = `${step * direction * sliderWidth}px`;
-   return sliderList.style.transform += `translateX(${sliderCoordinate})`;
+  const setCoordinate = (step, direction, position) => {
+     const sliderCoordinate = `${step * direction * sliderWidth + position * direction}px`;
+     return sliderList.style.transform += `translateX(${sliderCoordinate})`;
   }
+
   backButton.addEventListener('click', () => {
-    setCoordinate(STEP, BACK_DIRECTION);
+    setCoordinate(STEP, BACK_DIRECTION, START_POSITION);
   })
   nextButton.addEventListener('click', () => {
-    setCoordinate(STEP, FORWARD_DIRECTION);
+    setCoordinate(STEP, FORWARD_DIRECTION, START_POSITION);
+  })
+  sliderList.addEventListener('transitionstart', () => {
+    backButton.setAttribute('disabled', 'disabled')
+    backButton.classList.add('slider__button--disabled')
+    nextButton.setAttribute('disabled', 'disabled')
+    nextButton.classList.add('slider__button--disabled')
+  })
+  sliderList.addEventListener('transitionend', () => {
+    backButton.removeAttribute('disabled')
+    backButton.classList.remove('slider__button--disabled')
+    nextButton.removeAttribute('disabled')
+    nextButton.classList.remove('slider__button--disabled')
   })
 }
 
