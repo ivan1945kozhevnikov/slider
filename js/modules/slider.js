280f8ee6254ed2a80932a58c0af2a9bbb6890e9e
Ivan <kozhevnikovivan86@gmail.com>
2022-12-29 13:35:59 +0300

    let's implement the ability to scroll modify

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -2,38 +2,44 @@ import { getTranslateX } from '../utils/getTranslateX.js'
 const STEP = 1;
 const FORWARD_DIRECTION = -1;
 const BACK_DIRECTION = 1;
+const SLIDES_PAGE = 5;
 
 const slider = (nodeSlider) => {
   const backButton = nodeSlider.querySelector('.slider__button--prev');
   const nextButton = nodeSlider.querySelector('.slider__button--next');
   const sliderList = nodeSlider.querySelector('.slider__list');
   const sliderItem = nodeSlider.querySelector('.goods__item');
+  const items = nodeSlider.querySelectorAll('.goods__item');
   const sliderWidth = sliderItem.offsetWidth;
+  const slidesWidth = sliderWidth * items.length;
   const startPosition = getTranslateX(nodeSlider);
-  const maxPosition = sliderWidth * 15;
+  const maxPosition = slidesWidth - sliderWidth * SLIDES_PAGE;
+  const stepWidth = sliderWidth * STEP;
   const minPosition = startPosition;
-  let position = startPosition;
+  let currentPosition = startPosition;
 
-  const setCoordinate = (step, direction, start) => {
-    const sliderCoordinate = step * direction * sliderWidth + start;
+  const setCoordinate = (direction, start) => {
+    const sliderCoordinate = direction * stepWidth + start;
     sliderList.style.transform += `translateX(${sliderCoordinate}px)`;
   }
 
   backButton.addEventListener('click', () => {
-    position -= sliderWidth * STEP;
-    if (position === minPosition) {
+    currentPosition -= stepWidth;
+    if (currentPosition === minPosition) {
       backButton.classList.add('slider__button--hidden');
+    } else {
+      nextButton.classList.remove('slider__button--hidden');
     }
-    nextButton.classList.remove('slider__button--hidden');
-    setCoordinate(STEP, BACK_DIRECTION, startPosition);
+    setCoordinate(BACK_DIRECTION, startPosition);
   })
   nextButton.addEventListener('click', () => {
-    position += sliderWidth * STEP;
-    if (position === maxPosition) {
+    currentPosition += stepWidth;
+    if (currentPosition === maxPosition) {
       nextButton.classList.add('slider__button--hidden');
+    } else {
+      backButton.classList.remove('slider__button--hidden');
     }
-    backButton.classList.remove('slider__button--hidden');
-    setCoordinate(STEP, FORWARD_DIRECTION, startPosition);
+    setCoordinate(FORWARD_DIRECTION, startPosition);
   })
   sliderList.addEventListener('transitionstart', () => {
     backButton.setAttribute('disabled', '');
