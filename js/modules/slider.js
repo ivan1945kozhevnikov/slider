9edb6a82d82c613b1889d5d06f8a5418fcb60a3e
Ivan <kozhevnikovivan86@gmail.com>
2022-12-29 11:37:11 +0300

    let's implement the ability to scroll forward only to the last slide and backward only to the first slide

--- js/modules/slider.js
+++ js/modules/slider.js
@@ -3,7 +3,6 @@ const STEP = 1;
 const FORWARD_DIRECTION = -1;
 const BACK_DIRECTION = 1;
 
-
 const slider = (nodeSlider) => {
   const backButton = nodeSlider.querySelector('.slider__button--prev');
   const nextButton = nodeSlider.querySelector('.slider__button--next');
@@ -11,6 +10,9 @@ const slider = (nodeSlider) => {
   const sliderItem = nodeSlider.querySelector('.goods__item');
   const sliderWidth = sliderItem.offsetWidth;
   const startPosition = getTranslateX(nodeSlider);
+  const maxPosition = sliderWidth * 15;
+  const minPosition = startPosition;
+  let position = startPosition;
 
   const setCoordinate = (step, direction, start) => {
     const sliderCoordinate = step * direction * sliderWidth + start;
@@ -18,22 +20,32 @@ const slider = (nodeSlider) => {
   }
 
   backButton.addEventListener('click', () => {
+    position -= sliderWidth * STEP;
+    if (position === minPosition) {
+      backButton.classList.add('slider__button--hidden');
+    }
+    nextButton.classList.remove('slider__button--hidden');
     setCoordinate(STEP, BACK_DIRECTION, startPosition);
   })
   nextButton.addEventListener('click', () => {
+    position += sliderWidth * STEP;
+    if (position === maxPosition) {
+      nextButton.classList.add('slider__button--hidden');
+    }
+    backButton.classList.remove('slider__button--hidden');
     setCoordinate(STEP, FORWARD_DIRECTION, startPosition);
   })
   sliderList.addEventListener('transitionstart', () => {
-    backButton.setAttribute('disabled', '')
-    backButton.classList.add('slider__button--disabled')
-    nextButton.setAttribute('disabled', '')
-    nextButton.classList.add('slider__button--disabled')
+    backButton.setAttribute('disabled', '');
+    backButton.classList.add('slider__button--disabled');
+    nextButton.setAttribute('disabled', '');
+    nextButton.classList.add('slider__button--disabled');
   })
   sliderList.addEventListener('transitionend', () => {
-    backButton.removeAttribute('disabled')
-    backButton.classList.remove('slider__button--disabled')
-    nextButton.removeAttribute('disabled')
-    nextButton.classList.remove('slider__button--disabled')
+    backButton.removeAttribute('disabled');
+    backButton.classList.remove('slider__button--disabled');
+    nextButton.removeAttribute('disabled');
+    nextButton.classList.remove('slider__button--disabled');
   })
 }
 
