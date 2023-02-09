505a1d918978b794d27cf83476f2172fa58e7641
Ivan <kozhevnikovivan86@gmail.com>
2023-02-09 23:00:15 +0300

    first commit

--- js/utils/getTranslateX.js
+++ js/utils/getTranslateX.js
@@ -1,8 +1,8 @@
 const getTranslateX = (nodeSlider) => {
   const sliderList = nodeSlider.querySelector('.slider__list');
   const style = getComputedStyle(sliderList);
-  const matrix = new WebKitCSSMatrix(style.transform);
+  const matrix = new DOMMatrix(style.transform);
   return matrix.m41;
-}
+};
 
-export { getTranslateX }
+export default getTranslateX;
