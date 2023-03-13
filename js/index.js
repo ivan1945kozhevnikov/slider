323aba78a1e336e5f5db2f0795e3010c63ae8634
Ivan Kozhevnikov <kozhevnikovivan86@gmail.com>
2023-03-13 17:40:48 +0300

    Installed prettier and eslint

--- js/index.js
+++
@@ -1,7 +0,0 @@
-import { slider } from './modules/slider.js'
-
-const settings = {
-  step : 2,
-}
-const nodeSlider = document.querySelector('.slider');
-slider(nodeSlider, settings);
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,4 +1,4 @@
-import { getTranslateX } from '../utils/getTranslateX.js'
+import { getTranslateX } from "../utils/getTranslateX.js"
 
 //настройка слайдера по умолчанию
 const DEFAULT_SETTING = {step : 1, infinite : false};
@@ -11,15 +11,15 @@ const SLIDES_PAGE = 5;
 
 const slider = (nodeSlider, {step = 1, infinite = false} = DEFAULT_SETTING) => {
   //кнопка назад
-  const backButton = nodeSlider.querySelector('.slider__button--prev');
+  const backButton = nodeSlider.querySelector(".slider__button--prev");
   //кнопка вперед
-  const nextButton = nodeSlider.querySelector('.slider__button--next');
+  const nextButton = nodeSlider.querySelector(".slider__button--next");
   //список слайдов
-  const sliderList = nodeSlider.querySelector('.slider__list');
+  const sliderList = nodeSlider.querySelector(".slider__list");
   //слайд
-  const sliderItem = nodeSlider.querySelector('.goods__item');
+  const sliderItem = nodeSlider.querySelector(".goods__item");
   //массив слайдов
-  const sliderItems = nodeSlider.querySelectorAll('.goods__item');
+  const sliderItems = nodeSlider.querySelectorAll(".goods__item");
   //ширина слайда
   const sliderWidth = sliderItem.offsetWidth;
   //ширина всех слайдов
@@ -50,14 +50,14 @@ const slider = (nodeSlider, {step = 1, infinite = false} = DEFAULT_SETTING) => {
   }
 
   const toggleControlVisibility = (control) => {
-    control.classList.toggle('slider__button--hidden');
+    control.classList.toggle("slider__button--hidden");
   }
   //если infinite = false, тогда переключает видимость кнопки назад
   if (!infinite) {
     toggleControlVisibility(backButton);
   }
 
-  backButton.addEventListener('click', () => {
+  backButton.addEventListener("click", () => {
     currentPosition += calculatePosition(BACK_DIRECTION);
     //если infinite = false
     if (!infinite) {
@@ -74,7 +74,7 @@ const slider = (nodeSlider, {step = 1, infinite = false} = DEFAULT_SETTING) => {
     setPosition(currentPosition);
   })
 
-  nextButton.addEventListener('click', () => {
+  nextButton.addEventListener("click", () => {
     currentPosition += calculatePosition(FORWARD_DIRECTION);
      //если infinite = false
     if (!infinite) {
@@ -91,17 +91,17 @@ const slider = (nodeSlider, {step = 1, infinite = false} = DEFAULT_SETTING) => {
     setPosition(currentPosition);
   })
 
-  sliderList.addEventListener('transitionstart', () => {
-    backButton.setAttribute('disabled', '');
-    backButton.classList.add('slider__button--disabled');
-    nextButton.setAttribute('disabled', '');
-    nextButton.classList.add('slider__button--disabled');
+  sliderList.addEventListener("transitionstart", () => {
+    backButton.setAttribute("disabled", "");
+    backButton.classList.add("slider__button--disabled");
+    nextButton.setAttribute("disabled", "");
+    nextButton.classList.add("slider__button--disabled");
   })
-  sliderList.addEventListener('transitionend', () => {
-    backButton.removeAttribute('disabled');
-    backButton.classList.remove('slider__button--disabled');
-    nextButton.removeAttribute('disabled');
-    nextButton.classList.remove('slider__button--disabled');
+  sliderList.addEventListener("transitionend", () => {
+    backButton.removeAttribute("disabled");
+    backButton.classList.remove("slider__button--disabled");
+    nextButton.removeAttribute("disabled");
+    nextButton.classList.remove("slider__button--disabled");
   })
 }
 
